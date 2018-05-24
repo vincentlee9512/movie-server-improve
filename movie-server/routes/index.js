4fc9bb6dbadf73b73bba8cf157809f7e28b609d0
Vincent-xps <li000053@stthomas.edu>
2018-05-24 15:32:44 -0500

    individual movie page is working, except for the poster

--- movie-server/routes/index.js
+++ movie-server/routes/index.js
@@ -55,4 +55,62 @@ router.get('/search/:content', function (req, res, next) {
 
 });
 
+router.get('/individual/:uid', function (req, res, next) {
+  let uid = req.params.uid;
+  let sql;
+
+  //conditional statement
+  //check if the uid is for title or person
+  if(uid.substring(0,2) === "tt"){
+    //is title
+    sql = "SELECT DISTINCT Titles.tconst, Titles.primary_title, Titles.title_type, Titles.start_year, " +
+        "Titles.end_year, Titles.runtime_minutes, Titles.genres, Principals.ordering, Principals.nconst, " +
+        "Ratings.average_rating, Ratings.num_votes, Names.primary_name, Crew.directors, Crew.writers " +
+        "FROM Titles, Principals, Ratings, Names, Crew " +
+        "WHERE Titles.tconst = Principals.tconst AND Principals.tconst = Ratings.tconst AND Titles.tconst = Crew.tconst " +
+        " AND Titles.tconst = \"" + uid + "\"" +
+        " AND Principals.nconst = Names.nconst " +
+        " ORDER BY Principals.ordering;";
+  }else if (uid.substring(0,2) === "nm"){
+      sql = "SELECT * FROM Names WHERE nconst = \"" + uid +"\"";
+  }
+
+  var db_p = promises_module.db_promise(sql);
+
+  db_p.then( (data) => {
+
+    if(data.length === 0){
+      res.render('individual', { html_code: 'no information for this movie'});
+    }else{
+      let html_code = "";
+      let returnObj = {};
+
+      if(uid.substring(0,2) === "tt"){
+        returnObj = format_module.title_individual(data);
+        html_code = returnObj.html_code;
+
+        let directors_db_p = promises_module.db_promise(returnObj.directors_sql);
+        let writers_db_p = promises_module.db_promise(returnObj.directors_sql);
+
+        Promise.all([directors_db_p, writers_db_p]).then((data_arr) => {
+
+          let directors_list = format_module.people_list(data_arr[0]);
+          let writers_list = format_module.people_list(data_arr[1]);
+
+          html_code = html_code.replace('***directors***', directors_list);
+          html_code = html_code.replace('***writers***', writers_list);
+
+          res.render('individual', { html_code: html_code});
+        });
+      }else{
+        returnObj = {};
+      }
+
+    }
+  }).catch((error) => {
+    console.log(error);
+  });
+
+});
+
 module.exports = router;
